35439ee04f22220f8c577abb3e2e9250625efabe
cakajervisa <ervisa.cakaj@gmail.com>
2023-09-20 22:32:41 +0200

    Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: ` "nav "  " main"  `,
+        lg: ` "nav nav"  "aside main"  `,
+      }}
+    >
+      <GridItem area="nav" bg={"blue.100"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg={"blue.400"}>
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg={"blue.700"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
 
 export default App;
