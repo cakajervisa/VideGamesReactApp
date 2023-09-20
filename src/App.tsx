1d6ab9d63ce981da34ad207c4372c7d435b98cdd
cakajervisa <ervisa.cakaj@gmail.com>
2023-09-20 23:13:14 +0200

    Implement Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       }}
     >
       <GridItem area="nav" bg={"blue.100"}>
-        Nav
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg={"blue.400"}>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Text>text</Text>
+      <Text>text</Text>
+      <Text>text</Text>
+      <Text>text</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
